7950b44f466240fc462f159761383ad8661cee9f
Jan Groth <jan.groth.de@gmail.com>
2024-11-13 16:38:06 +1100

    tmux

--- scripts/20_setup_tmux.sh
+++ scripts/20_setup_tmux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-[ -n "$DOT_DEBUG" ] && set -x
+[ -n "$DOT_DEBUG" ] && set +x
 
 . "$(dirname "$0")/_config.sh"
 
@@ -12,6 +12,7 @@ cp -f "${DOT_ROOT}/files/tmux/tmux.conf" $HOME/.tmux.conf
 
 # update remote dependencies
 if [ -n "$DOT_REINSTALL" ] || [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    echo "Installing tmux plugins..."
     rm -rf $HOME/.tmux/plugins/tpm
     git clone --depth 1 https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
